fb80a46008a948599eed4d99c478e67668112047
Jules Caesaris <julescaesaris@gmail.com>
2023-03-20 11:28:14 +0100

    #1: Initialize repo

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: false,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  endOfLine: "auto",
+  parser: "typescript"
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# user-management
+# User Management app
+
+Simple user management app using Next.js, React, Typescript, Redux Toolkit and Styled Components
