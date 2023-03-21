d01b5b86b215e48d6757cf44372e4616d8f62488
Przemyslaw Bok <przemyslawbok84@gmail.com>
2023-03-21 20:56:48 +0100

    #11: Added json-server with mock data

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,8 +1,7 @@
-import { User } from '@/common'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
   tagTypes: ['User'],
-  endpoints: (builder) => ({})
+  endpoints: () => ({})
 })
